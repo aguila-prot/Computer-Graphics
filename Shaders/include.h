583b0a961f4c2ae6f260f57a287f44955607f351
Arthur Arya <aguila.prot@gmail.com>
2018-07-25 12:55:10 +0300

    Commit 1

--- Shaders/include.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/foreach.hpp>
-#include <GL/glew.h>
-#include <GL/glut.h>
-#include <GLFW/glfw3.h>
